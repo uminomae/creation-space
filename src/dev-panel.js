78c4ed34a11ae8b4ae81cf4b9aa313ec8c9c7b2d
uminomae <uminomae@gmail.com>
2026-02-24 18:53:50 +0900

    Revert "Prune 3 unused hold graphic objects (refs #17)"
    This reverts commit ac836bf7583212f3b2ed1db2c93817f3bcf3878a.

--- src/dev-panel.js
+++ src/dev-panel.js
@@ -379,10 +379,6 @@ const PARAM_GROUPS = [
 const HIDDEN_GROUP_IDS_BY_VARIANT = {
     hold: new Set([
         'plasma',
-        'creationGlobal',
-        'creationLink1',
-        'creationLink2',
-        'creationLink3',
     ]),
     wabi: new Set([
         'creationLink2',
